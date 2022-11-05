22ac3b7c2fc1a2b5a8ffbaa1e8a4462e13a23fc0
Andrew Keates <akeates2@gmail.com>
2022-11-05 13:32:43 -0600

    add start menu outline

--- movieRentalApp/Form1.Designer.cs
+++ movieRentalApp/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace movieRentalApp
 {
-    partial class Form1
+    partial class startMenu
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.startMenuHeader = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.signupButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startMenuHeader
+            // 
+            this.startMenuHeader.AutoSize = true;
+            this.startMenuHeader.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.startMenuHeader.Location = new System.Drawing.Point(19, 20);
+            this.startMenuHeader.Name = "startMenuHeader";
+            this.startMenuHeader.Size = new System.Drawing.Size(694, 54);
+            this.startMenuHeader.TabIndex = 0;
+            this.startMenuHeader.Text = "Welcome to ____ Video Rental Service ";
+            this.startMenuHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.startMenuHeader.Click += new System.EventHandler(this.startMenuHeader_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.loginButton.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.loginButton.Location = new System.Drawing.Point(95, 202);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(180, 51);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Log In";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // signupButton
+            // 
+            this.signupButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.signupButton.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.signupButton.Location = new System.Drawing.Point(434, 202);
+            this.signupButton.Name = "signupButton";
+            this.signupButton.Size = new System.Drawing.Size(180, 51);
+            this.signupButton.TabIndex = 2;
+            this.signupButton.Text = "Sign Up";
+            this.signupButton.UseVisualStyleBackColor = true;
+            this.signupButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // startMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(713, 382);
+            this.Controls.Add(this.signupButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.startMenuHeader);
+            this.Name = "startMenu";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label startMenuHeader;
+        private Button loginButton;
+        private Button signupButton;
     }
 }
--- movieRentalApp/Form1.cs
+++ movieRentalApp/Form1.cs
@@ -1,10 +1,30 @@
 namespace movieRentalApp
 {
-    public partial class Form1 : Form
+    public partial class startMenu : Form
     {
-        public Form1()
+        public startMenu()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startMenuHeader_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
