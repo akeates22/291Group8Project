f364a6265493a250869e641a2d1bdc3115d44b6a
Andrew Keates <akeates2@gmail.com>
2022-11-23 23:02:32 -0700

    connect to sql server

--- movieRentalApp/Signup.cs
+++ movieRentalApp/Signup.cs
@@ -9,11 +9,14 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Data.Common;
 
 namespace movieRentalApp
 {
     public partial class Signup : Form
     {
+        public SqlConnection sqlConnection;
+        public SqlCommand cmd;
         public Signup()
         {
             InitializeComponent();
@@ -29,6 +32,9 @@ namespace movieRentalApp
             string city = cityInput.Text;
             string province = provinceInput.Text;
             string email = emailInput.Text;
+            
+            DateTime dt = DateTime.Now;
+            string creationDate = dt.ToShortDateString();
 
             try
             {
@@ -36,7 +42,8 @@ namespace movieRentalApp
                 int phone = Int32.Parse(phoneInput.Text);
             }
             
-            catch {
+            catch 
+            {
                 MessageBox.Show("Invalid entries, please try again", "Registration Failed");
                 this.Close();
                 return;
@@ -64,39 +71,46 @@ namespace movieRentalApp
 
             }
 
-            // look at last customer added to the database, add 1 to their ID
-            // for now, just use a hardcoded ID
-            int CID = 1;
-
-            // connect to database
             string connectionString = "Server = ANDREWS-PC; Database = 291Project; Trusted_Connection = Yes";
             SqlConnection myConnection = new SqlConnection(connectionString);
 
             try
             {
-                
-
+                // connect to database
                 myConnection.Open();
-
                 SqlCommand cmd = new SqlCommand();
+                cmd.Connection = myConnection;
 
-                cmd.CommandText = "";
+                // look at last customer added to the database, add 1 to their ID
+                // for now, just use a hardcoded ID
+                int CID = 1;
+
+                cmd.CommandText = "insert into customers values (" + CID.ToString() + ",'" + accType + "','" + fname + 
+                                  "','" + lname + "','" + address + "','" + city + "','" + province + "','" + 
+                                  postalCode + "'," + phoneInput.Text + ",'" + email + "','" + creationDate + "'," + 
+                                  ccNumberInput.Text + "," + "0.0" + ")";
 
                 cmd.ExecuteNonQuery();
+                myConnection.Close();
+
+                MessageBox.Show(String.Format("Registration successful, your Customer ID is: {0}", CID), "Registration Successful");
+                Login loginMenu = new Login(CID.ToString());
+                loginMenu.Show();
+                this.Close();
+
+                return;
             }
-            catch
+            catch (Exception ex)
             {
                 MessageBox.Show("Unable to register user, try again later", "Registration Error");
+                MessageBox.Show(ex.Message);
+
                 myConnection.Close();
                 this.Close();
                 return;
             }
-            myConnection.Close();
 
-            MessageBox.Show(String.Format("Registration successful, your Customer ID is: {0}", CID), "Registration Successful");
-            Login loginMenu = new Login(CID.ToString());
-            this.Close();
-
+            
         }
     }
 }
